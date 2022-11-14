8a88d4ca5d5e82ce32fca378546283602ac5939e
MohamedShokrani <MohamedShokrani55@gmail.com>
2022-11-15 00:52:12 +0200

    output edit initial ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavComponent } from './home-page/nav/nav.component';
+import { MainSectionComponent } from './home-page/main-section/main-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavComponent,
+    MainSectionComponent
   ],
   imports: [
     BrowserModule,
